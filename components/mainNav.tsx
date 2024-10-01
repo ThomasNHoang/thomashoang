c2739bec30d9f5132e84cda725d371a8c7de56bc
Thomas Hoang <139909659+ThomasNHoang@users.noreply.github.com>
2024-10-01 19:07:57 +0000

    Added main navbar

---
+++ components/mainNav.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export function MainNavigation() {
+  const pathname = usePathname();
+
+  return (
+    <div className="mr-4 hidden md:flex">
+      <Link href="/" className="mr-4 flex items-center space-x-2 lg:mr-6">
+        <span className="hidden font-bold lg:inline-block">
+          Thomas Hoang
+        </span>
+      </Link>
+      <nav className="flex items-center gap-4 text-sm lg:gap-6">
+        {/* Todo: Add Navlinks Dynamically */}
+        <Link
+          href="/"
+          className={cn(
+            "transition-colors hover:text-foreground/80",
+            pathname === "/" ? "text-foreground" : "text-foreground/60"
+          )}
+        >
+          Placeholder
+        </Link>
+        <Link
+          href="/"
+          className={cn(
+            "transition-colors hover:text-foreground/80",
+            pathname === "/" ? "text-foreground" : "text-foreground/60"
+          )}
+        >
+          Placeholder
+        </Link>
+        <Link
+          href="/"
+          className={cn(
+            "transition-colors hover:text-foreground/80",
+            pathname === "/" ? "text-foreground" : "text-foreground/60"
+          )}
+        >
+          Placeholder
+        </Link>
+        <Link
+          href="/"
+          className={cn(
+            "transition-colors hover:text-foreground/80",
+            pathname.startsWith('/') ? "text-foreground" : "text-foreground/60"
+          )}
+        >
+          Placeholder
+        </Link>
+      </nav>
+    </div>
+  )
+}
--- components/navHeader.tsx
+++ components/navHeader.tsx
@@ -1,8 +1,10 @@
+import { MainNavigation } from "@/components/mainNav"
+
 export function NavigationHeader() {
   return (
     <header className="sticky top-0 z-10 w-full border-border/50 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-14 max-w-screen-2xl items-center">
-        {/* Todo: Main */}
+        <MainNavigation />
         {/* Todo: Mobile */}
         Hello World
       </div>
