a057c02997a048e496b95abb2d1179cfb2e3e7d1
Thomas Hoang <139909659+ThomasNHoang@users.noreply.github.com>
2024-10-02 17:40:12 +0000

    Added type in schema

--- schema.ts
+++ schema.ts
@@ -2,4 +2,6 @@ import { z } from "zod";
 
 export const loginFormSchema = z.object({
   email: z.string().email({message: "Please enter a valid email address"})
-})
+});
+
+export type loginSchemaType = z.infer<typeof loginFormSchema>;
