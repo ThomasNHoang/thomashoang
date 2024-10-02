ee32926f510ad1ce88bb9bf2ad1dc2544cbc3f01
Thomas Hoang <139909659+ThomasNHoang@users.noreply.github.com>
2024-10-02 15:10:25 +0000

    Added server actions for github codespace

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverActions: {
+      allowedForwardedHosts: [
+        "localhost:3000",
+        "refactored-waffle-69gvw7j59v6w25g9p-3000.app.github.dev",
+      ],
+      allowedOrigins: [
+        "localhost:3000",
+        "refactored-waffle-69gvw7j59v6w25g9p-3000.app.github.dev",
+      ],
+    },
+  },
+};
 
 export default nextConfig;
