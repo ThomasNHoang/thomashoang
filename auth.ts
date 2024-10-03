ea65af4225bad6382268f8de40200dce49a9884e
Thomas Hoang <139909659+ThomasNHoang@users.noreply.github.com>
2024-10-03 23:11:39 +0000

    Fixed email name

--- auth.ts
+++ auth.ts
@@ -5,21 +5,23 @@ import Google from "next-auth/providers/google";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import Nodemailer from "next-auth/providers/nodemailer";
 
+const serverConfig = {
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.SMTP_EMAIL,
+    pass: process.env.SMTP_EMAIL_PASS
+  }
+}
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
   ...authConfig,
   providers: [
     Google,
     Nodemailer({
-      server: {
-        host: "smtp.gmail.com",
-        port: 465,
-        secure: true,
-        auth: {
-          user: process.env.SMTP_EMAIL,
-          pass: process.env.SMTP_EMAIL_PASS
-        },
-        from: `Email Service <${process.env.SMTP_EMAIL}>`
-      },
+      server: serverConfig,
+      from: `Email Service <${process.env.SMTP_EMAIL}>`
     })
   ],
   adapter: PrismaAdapter(prisma),
