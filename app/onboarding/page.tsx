cf6e6750a7981082e0dd6664e08b9560cbde1748
Thomas Hoang <139909659+ThomasNHoang@users.noreply.github.com>
2024-10-11 20:04:05 -0400

    Change theme (#7)
    * Changed to slate theme

--- app/onboarding/page.tsx
+++ app/onboarding/page.tsx
@@ -20,12 +20,12 @@ export default async function OnboardingPage() {
         <OnboardingForm />
       </div>
       <div className="mt-4 text-center">
-        <p className="text-md">
+        <p className="text-md text-muted-foreground">
           Don&apos;t want to do this now? You can complete it later in settings.
         </p>
         <Link
           href={DEFAULT_LOGIN_REDIRECT}
-          className="hover:underline text-blue-500 hover:text-blue-600"
+          className="hover:underline underline-offset-4"
         >
           Go to dashboard
         </Link>
