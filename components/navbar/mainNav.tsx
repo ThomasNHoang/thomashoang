dc3e0989fafb8547578c7d80790a334794ab53f3
ThomasNHoang <139909659+ThomasNHoang@users.noreply.github.com>
2024-10-11 22:20:38 -0500

    Dynamic links

--- components/navbar/mainNav.tsx
+++ components/navbar/mainNav.tsx
@@ -2,6 +2,7 @@
 
 import Link from "next/link";
 import { cn } from "@/lib/utils";
+import { navbarRoutes } from "@/routes";
 import { RxGlobe } from "react-icons/rx";
 import { usePathname } from "next/navigation";
 
@@ -15,43 +16,18 @@ export function MainNavigation() {
         <span className="hidden font-bold lg:inline-block">Thomas Hoang</span>
       </Link>
       <nav className="flex items-center gap-4 text-sm lg:gap-6">
-        {/* Todo: Add Navlinks Dynamically */}
-        <Link
-          href="/"
-          className={cn(
-            "transition-colors hover:text-foreground/80",
-            pathname === "/" ? "text-foreground" : "text-foreground/60",
-          )}
-        >
-          Placeholder
-        </Link>
-        <Link
-          href="/"
-          className={cn(
-            "transition-colors hover:text-foreground/80",
-            pathname === "/" ? "text-foreground" : "text-foreground/60",
-          )}
-        >
-          Placeholder
-        </Link>
-        <Link
-          href="/"
-          className={cn(
-            "transition-colors hover:text-foreground/80",
-            pathname === "/" ? "text-foreground" : "text-foreground/60",
-          )}
-        >
-          Placeholder
-        </Link>
-        <Link
-          href="/"
-          className={cn(
-            "transition-colors hover:text-foreground/80",
-            pathname.startsWith("/") ? "text-foreground" : "text-foreground/60",
-          )}
-        >
-          Placeholder
-        </Link>
+        {navbarRoutes.map((item, index) => (
+          <Link
+            key={index}
+            href={item.href}
+            className={cn(
+              "transition-colors hover:text-foreground/80",
+              item.matcher(pathname) ? "text-foreground" : "text-foreground/60",
+            )}
+          >
+            {item.title}
+          </Link>
+        ))}
       </nav>
     </div>
   );
